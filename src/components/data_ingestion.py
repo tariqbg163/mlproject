dfb80103712f173ee48fb7a7528dcbd67a9f9c14
tariqg163 <tariqbg163@gmail.com>
2026-02-25 12:07:53 +0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:  # this is the class that tells where our data will be stored.
@@ -66,5 +68,8 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transforamtion = DataTransformation()
-    data_transforamtion.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transforamtion.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
             
